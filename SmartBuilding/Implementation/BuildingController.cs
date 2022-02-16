73bbb0a7947c9bfee2d57eb08da2ae700d53555e
Tom O'Sullivan <tom@tomdotbat.dev>
2022-02-16 10:02:21 +0000

    feat: add stub impl of BuildingController

---
+++ SmartBuilding/Implementation/BuildingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SmartBuilding.Implementation
+{
+    public class BuildingController : IBuildingController
+    {
+        public string GetCurrentState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetCurrentState(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetBuildingID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBuildingID(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStatusReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
