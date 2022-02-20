0a4e39137a712bb0627c6b9b4352496189a415e6
Tom O'Sullivan <tom@tomdotbat.dev>
2022-02-20 17:14:10 +0000

    feat: implement the FireAlarmManager

---
+++ SmartBuilding/Implementation/FireAlarmManager.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartBuilding.Implementation
+{
+    public class FireAlarmManager : Manager<IFireAlarm>, IFireAlarmManager
+    {
+        /// <summary>
+        /// Instantiates a FireAlarmManager and sets the manager type.
+        /// </summary>
+        public FireAlarmManager() : base("FireAlarm") { }
+
+        /// <summary>
+        /// Sets active state of all managed fire alarms.
+        /// </summary>
+        /// <param name="isActive">True if the fire alarm is active, false if not.</param>
+        public void SetAlarm(bool isActive)
+        {
+            // ReSharper disable once PossibleInvalidCastExceptionInForeachLoop
+            foreach (IFireAlarm fireAlarm in _devices)
+            {
+                fireAlarm.SetActive(isActive);
+            }
+        }
+    }
+}
