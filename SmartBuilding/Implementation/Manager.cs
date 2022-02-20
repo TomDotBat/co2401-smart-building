a85863cb89e2226bc734eced434d7ea54a1d2fdf
Tom O'Sullivan <tom@tomdotbat.dev>
2022-02-20 17:51:53 +0000

    feat: add validation to the Manager implementation

--- SmartBuilding/Implementation/Manager.cs
+++ SmartBuilding/Implementation/Manager.cs
@@ -1,19 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SmartBuilding.Implementation
 {
+    /// <summary>
+    /// Abstract class to implement IManager methods.
+    /// </summary>
     public abstract class Manager : IManager
     {
         /// <summary>
-        /// Instantiates a Manager with a device type used by GetStatus().
+        /// Instantiates a Manager with the device type "Unknown".
+        /// This constructor shouldn't normally exist, but in this
+        /// case it does for testing purposes.
+        /// </summary>
+        public Manager() : this("Unknown") { }
+        
+        /// <summary>
+        /// Instantiates a Manager with the given device type.
         /// </summary>
         /// <param name="deviceType">The type of device being managed.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the device type is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when the device type is an empty string or whitespace.</exception>
         protected Manager(string deviceType)
         {
-            // Set the device type to the given value
-            // and default engineerRequired to false.
+            // The device type is null, throw an exception.
+            if (deviceType == null)
+            {
+                throw new ArgumentNullException(nameof(deviceType), "Argument Null Exception: Device type must be present.");
+            }
+        
+            // The device type is an empty string/whitespace, throw an exception.
+            if (String.IsNullOrWhiteSpace(deviceType))
+            {
+                throw new ArgumentException("Argument Exception: Device type cannot be empty or whitespace.");
+            }
+            
+            // Set the device type to the given value,
+            // default engineerRequired to false and
+            // initialise the devices list.
             DeviceType = deviceType;
             engineerRequired = false;
+            _devices = new List<IDevice>();
         }
 
         /// <summary>
@@ -47,6 +74,22 @@ namespace SmartBuilding.Implementation
             }
 
             return statusString;
+        }
+        
+        /// <summary>
+        /// Registers a device as a managed device.
+        /// </summary>
+        /// <param name="device">The device to manage.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the device is null.</exception>
+        public void RegisterDevice(IDevice device)
+        {
+            // The device is null, throw an exception.
+            if (device == null)
+            {
+                throw new ArgumentNullException(nameof(device), "Argument Null Exception: Cannot register null as a managed device.");
+            }
+            
+            _devices.Add(device);
         }
 
         protected readonly string DeviceType;
